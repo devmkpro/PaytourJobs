a5c4463a16c92776e15061d9b595815073e2de37
maike <maikeofc18@gmail.com>
2025-09-03 14:13:38 -0300

    Remove public_hostinger disk configuration from filesystem settings

--- config/filesystems.php
+++ config/filesystems.php
@@ -77,12 +77,4 @@ return [
         public_path('storage') => storage_path('app/public'),
     ],
 
-
-    'public_hostinger' => [
-        'driver' => 'local',
-        'root' => storage_path('app/public'),
-        'url' => env('APP_URL').'/public/storage',
-        'visibility' => 'public',
-    ],
-
 ];
